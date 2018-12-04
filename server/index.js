9f13e07a6a6b64b1740740714880909dba3b359a
Jaggu <khushboojagnani254@gmail.com>
2018-12-04 18:30:06 +0530

    made the auth flow

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const keys = require("./config/keys");
+require("./model/User");
 require("./services/passport"); //Since We only want it to execute once
 
+mongoose.connect(keys.mongoURI);
 const app = express();
 
 require("./routes/authRoutes")(app);
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,7 +1,8 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
-
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+const User = mongoose.model("users");
 
 // we are making a new instance for authentication with google
 passport.use(
@@ -12,8 +13,17 @@ passport.use(
             callbackURL: "/auth/google/callback"
         },
         (accessToken, refreshToken, profile, done) => {
-            console.log(accessToken);
-            console.log(refreshToken);
+            User.findOne({ googleId: profile.id }).then(existingUser => {
+                if (existingUser) {
+                    // we already have a record in the database
+                    done(null, existingUser);
+                } else {
+                    // we don't have the record in the database
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
+                }
+            });
         }
     )
 );
