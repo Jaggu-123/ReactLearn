132573e584c48329ddbb4bb13c297e49f2420f0e
Jaggu <khushboojagnani254@gmail.com>
2018-12-04 18:39:42 +0530

    setting cookie Session to use with express

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
 
 const keys = require("./config/keys");
 require("./model/User");
@@ -7,6 +9,16 @@ require("./services/passport"); //Since We only want it to execute once
 
 mongoose.connect(keys.mongoURI);
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
