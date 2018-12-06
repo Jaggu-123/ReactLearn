4dcc2deb32456a724d79cfd2bcaad8cbc0688f30
Jaggu <khushboojagnani254@gmail.com>
2018-12-06 20:36:36 +0530

    Changing contents on header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,26 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li>
+                        <a href="/auth/google">Login With Google</a>
+                    </li>
+                );
+            default:
+                return (
+                    <li>
+                        <a href="/api/logout">Logout</a>
+                    </li>
+                );
+        }
+    }
+
     render() {
         return (
             <nav>
@@ -9,9 +29,7 @@ class Header extends Component {
                         Emaily
                     </a>
                     <ul className="right hid-on-mid-and-down">
-                        <li>
-                            <a href="#">Login With Google</a>
-                        </li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -19,4 +37,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
