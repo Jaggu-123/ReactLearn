faeac212106a7e66e91cd4c3937bc7aa12a1933b
Jaggu <khushboojagnani254@gmail.com>
2018-12-06 17:04:18 +0530

    refactor the async keyword

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-    try {
-        return function(dispatch) {
-            axios
-                .get("./api/current_user")
-                .then(res => dispatch({ type: FETCH_USER, payload: res }));
-        };
-    } catch (err) {
-        console.log("err1");
-    }
+// export const fetchUser = () => {
+//     return function(dispatch) {
+//         axios
+//             .get("./api/current_user")
+//             .then(res => dispatch({ type: FETCH_USER, payload: res }));
+//     };
+// };
+
+export const fetchUser = () => async dispatch => {
+    const res = await axios.get("./api/current_user");
+
+    dispatch({ type: FETCH_USER, payload: res });
 };
