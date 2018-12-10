ebf09a6207700f1c83bac166ba940f8dad75f01e
Jaggu <khushboojagnani254@gmail.com>
2018-12-10 20:02:13 +0530

    made the mailer object

--- server/middlewares/requireCredits.js
+++ server/middlewares/requireCredits.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (req.user.Credits < 1) {
+    if (req.user.credits < 1) {
         return res.status(403).send({ error: "Not enough Credits!" });
     }
 };
--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -18,11 +18,14 @@ module.exports = app => {
             recipients: recipients
                 .split(",")
                 .map(email => ({ email: email.trim() })),
-            _user: req.body.id,
+            _user: req.user.id,
             dateSent: Date.now()
         });
 
+        console.log(survey);
+
         //Great place to send an email
         const mailer = new Mailer(survey, surveyTemplate(survey));
+        mailer.send();
     });
 };
