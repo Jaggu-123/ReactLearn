bda2388f9a2f3b222972e1c3b15f1ae4ce41054b
Jaggu <khushboojagnani254@gmail.com>
2018-12-15 16:22:32 +0530

    restructured the webhook route

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -12,23 +12,24 @@ const Survey = mongoose.model("surveys");
 module.exports = app => {
     app.post("/api/surveys/webhooks", (req, res) => {
         const p = new Path("/api/surveys/:surveyId/:choice");
-        const events = _.map(req.body, event => {
-            const pathname = new URL(event.url).pathname;
+        const events = _.chain(req.body)
+            .map(event => {
+                const pathname = new URL(event.url).pathname;
 
-            const match = p.test(pathname);
-            if (match) {
-                return {
-                    email: event.email,
-                    surveyId: match.surveyId,
-                    choice: match.choice
-                };
-            }
-        });
+                const match = p.test(pathname);
+                if (match) {
+                    return {
+                        email: event.email,
+                        surveyId: match.surveyId,
+                        choice: match.choice
+                    };
+                }
+            })
+            .compact()
+            .uniqBy("email", "surveyId")
+            .value();
 
-        const compactEvents = _.compact(events);
-        const uniqueEvents = _.uniqBy(compactEvents, "email", "surveyId");
-
-        console.log(uniqueEvents);
+        console.log(events);
         res.send({});
     });
 
