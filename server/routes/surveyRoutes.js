9a94aa15556a1b0f9410a8374ca551e8ac8fc8f1
Jaggu <khushboojagnani254@gmail.com>
2018-12-15 17:16:00 +0530

    made route to get all surveys

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,6 +10,14 @@ const surveyTemplate = require("../services/emailTemplate/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
+    app.get("/api/surveys", requireLogin, async (req, res) => {
+        const surveys = await Survey.find({ _user: req.user.id }).select({
+            recipients: false
+        });
+
+        res.send(surveys);
+    });
+
     app.post("/api/surveys/webhooks", (req, res) => {
         const p = new Path("/api/surveys/:surveyId/:choice");
         _.chain(req.body)
