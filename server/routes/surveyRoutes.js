e0aa445890292cc48a6c23d08a797a88b98a928b
Jaggu <khushboojagnani254@gmail.com>
2018-12-15 15:47:25 +0530

    added route for webhook

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const Path = require("path-parser").default;
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -8,7 +11,24 @@ const Survey = mongoose.model("surveys");
 
 module.exports = app => {
     app.post("/api/surveys/webhooks", (req, res) => {
-        console.log(req.body);
+        const p = new Path("/api/surveys/:surveyId/:choice");
+        const events = _.map(req.body, event => {
+            const pathname = new URL(event.url).pathname;
+
+            const match = p.test(pathname);
+            if (match) {
+                return {
+                    email: event.email,
+                    surveyId: match.surveyId,
+                    choice: match.choice
+                };
+            }
+        });
+
+        const compactEvents = _.compact(events);
+        const uniqueEvents = _.uniqBy(compactEvents, "email", "surveyId");
+
+        console.log(uniqueEvents);
         res.send({});
     });
 
--- server/services/emailTemplate/surveyTemplate.js
+++ server/services/emailTemplate/surveyTemplate.js
@@ -7,10 +7,14 @@ module.exports = survey => {
                     <p>Please answer the following question:</p>
                     <p>${survey.body}</p>
                     <div>
-                        <a href="http://localhost:3000">Yes</a>
+                        <a href="http://localhost:3000/api/surveys/${
+                            survey.id
+                        }/yes">Yes</a>
                     </div>
                     <div>
-                        <a href="http://localhost:3000">No</a>
+                        <a href="http://localhost:3000/api/surveys/${
+                            survey.id
+                        }/no">No</a>
                     </div>
                 </div>
             </body>
