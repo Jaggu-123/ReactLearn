4d777859d30cc62ac989299ae22406c9969585a3
Jaggu <khushboojagnani254@gmail.com>
2018-12-15 17:32:49 +0530

    made the redux side for api/surveys

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as reduxForm } from "redux-form";
 import authReducers from "./authReducers";
+import surveysReducers from "./surveysReducers";
 
 export default combineReducers({
     auth: authReducers,
-    form: reduxForm
+    form: reduxForm,
+    surveys: surveysReducers
 });
