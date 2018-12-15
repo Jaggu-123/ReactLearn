1a011db02a27e68b8d699f0df506fa08935f0b24
Jaggu <khushboojagnani254@gmail.com>
2018-12-15 17:57:07 +0530

    made the react side

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SurveyList from "./surveys/SurveyList";
 
 const Dashboard = () => {
     return (
-        <div className="fixed-action-btn">
-            <Link
-                to="/surveys/new"
-                className="btn-floating btn-large waves-light waves-effect red"
-            >
-                <i className="material-icons">add</i>
-            </Link>
+        <div>
+            <SurveyList />
+            <div className="fixed-action-btn">
+                <Link
+                    to="/surveys/new"
+                    className="btn-floating btn-large waves-light waves-effect red"
+                >
+                    <i className="material-icons">add</i>
+                </Link>
+            </div>
         </div>
     );
 };
